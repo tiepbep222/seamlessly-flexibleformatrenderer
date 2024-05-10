2f7ba912a68533032ed9f5d87de5eba4f1ed5f95
tiepbep222 <thob0kholtpun2suy@hotmail.com>
2024-05-11 03:22:09 +0700

    feat task

--- index.js
+++ index.js
@@ -1,35 +1,13 @@
-const cycleSort = (arr) => {
-  for (let start = 0; start < arr.length - 1; start++) {
-    let item = arr[start];
-    let pos = start;
-    for (let i = start + 1; i < arr.length; i++) {
-      if (arr[i] < item) {
-        pos++;
-      }
-    }
-    if (pos === start) {
-      continue;
-    }
-    while (item === arr[pos]) {
-      pos++;
-    }
-    if (pos !== start) {
-      [item, arr[pos]] = [arr[pos], item];
-    }
-    while (pos !== start) {
-      pos = start;
-      for (let i = start + 1; i < arr.length; i++) {
-        if (arr[i] < item) {
-          pos++;
-        }
-      }
-      while (item === arr[pos]) {
-        pos++;
-      }
-      if (item !== arr[pos]) {
-        [item, arr[pos]] = [arr[pos], item];
-      }
-    }
+function maxProduct(nums) {
+  if (nums.length === 0) return 0;
+  let maxSoFar = nums[0];
+  let minSoFar = nums[0];
+  let maxProduct = nums[0];
+  for (let i = 1; i < nums.length; i++) {
+    const temp = maxSoFar;
+    maxSoFar = Math.max(nums[i], nums[i] * maxSoFar, nums[i] * minSoFar);
+    minSoFar = Math.min(nums[i], nums[i] * temp, nums[i] * minSoFar);
+    maxProduct = Math.max(maxProduct, maxSoFar);
   }
-  return arr;
-};
+  return maxProduct;
+}
