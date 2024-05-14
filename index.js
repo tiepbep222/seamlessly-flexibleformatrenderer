4a8a57315fb70d825d12a6d0027ac8237ffe900a
tiepbep222 <thob0kholtpun2suy@hotmail.com>
2024-05-14 13:25:18 +0700

    update task

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
-function pathSum(root, sum) {
-  if (!root) return false;
-  if (!root.left && !root.right && root.val === sum) return true;
-  return (
-    pathSum(root.left, sum - root.val) || pathSum(root.right, sum - root.val)
-  );
+function sortColors(nums) {
+  let left = 0;
+  let right = nums.length - 1;
+  let i = 0;
+  while (i <= right) {
+    if (nums[i] === 0) {
+      [nums[i], nums[left]] = [nums[left], nums[i]];
+      left++;
+      i++;
+    } else if (nums[i] === 2) {
+      [nums[i], nums[right]] = [nums[right], nums[i]];
+      right--;
+    } else {
+      i++;
+    }
+  }
 }
