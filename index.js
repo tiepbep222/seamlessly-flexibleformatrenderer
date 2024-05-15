6030503bb8626c3b14dca94fad97e9d3d0e30809
tiepbep222 <thob0kholtpun2suy@hotmail.com>
2024-05-15 15:18:56 +0700

    update aptos project

--- index.js
+++ index.js
@@ -1,17 +1,3 @@
-function sortColors(nums) {
-  let left = 0;
-  let right = nums.length - 1;
-  let i = 0;
-  while (i <= right) {
-    if (nums[i] === 0) {
-      [nums[i], nums[left]] = [nums[left], nums[i]];
-      left++;
-      i++;
-    } else if (nums[i] === 2) {
-      [nums[i], nums[right]] = [nums[right], nums[i]];
-      right--;
-    } else {
-      i++;
-    }
-  }
+function rotateArray(arr, n) {
+  return arr.slice(n).concat(arr.slice(0, n));
 }
